3e495e04708d216bb992cd7e171632ac9fd45f0f
PhungHuynh <huynhtrongphungse@gmail.com>
2024-04-20 14:41:20 +0700

    first commit

---
+++ my-love/src/test/java/com/love/mylove/MyLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.mylove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
